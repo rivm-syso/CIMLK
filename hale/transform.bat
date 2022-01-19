d8e0ddffb474d92b0fcbe44413c0c3d62645d19e
Michiel Geijer <michiel.geijer@rivm.nl>
2022-01-19 17:29:15 +0100

    Test hale-cli

--- hale/transform.bat
+++
@@ -1,1 +0,0 @@
-java -jar "C:\Program Files\wetransform\HALE\plugins\org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar" -application hale.transform -project IMLKtest.halex -source 20211117_OT_Codelijst_2022_OIN_Bronhouders_KvK_Type_CBScode_END_aggl_MG.xlsx -source segment_amersfoort_g307_mt2020_j2019_rpgw_s.csv -target output.gml -preset GML -reportsOut imlk.log -stacktrace
