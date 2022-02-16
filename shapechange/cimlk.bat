8d6487ecc8158c38e7c2a17dfbf6899b5f3f6d2e
Michiel Geijer <michiel.geijer@rivm.nl>
2022-02-16 14:46:27 +0100

    Update shapechange scripts

--- shapechange/cimlk.bat
+++ shapechange/cimlk.bat
@@ -1,3 +1,4 @@
-java -jar "c:\Users\dgree\OneDrive - ArchiXL BV\ShapeChange\ShapeChange-2.10.0.jar" -Dfile.encoding=UTF-8 -c cimlk.xml
+set /p SHAPECHANGE_JAVA=<shapechange_java.txt
+java -jar %SHAPECHANGE_JAVA% -Dfile.encoding=UTF-8 -c cimlk.xml
 copy results\INPUT\imlk.xsd .
 rmdir /s /q results
