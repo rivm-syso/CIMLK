c69c038be3ca8a6cbce817720db2b5525b66273f
Danny Greefhorst <46052032+dgreefhorst@users.noreply.github.com>
2022-03-14 11:12:52 +0100

    Wegverkeer import ontwikkeld

---
+++ hale/importwegverkeer.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\wetransform\HALE\jre\bin\java" -jar "C:\Program Files\wetransform\HALE\plugins\org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar" -application hale.transform -project "Wegverkeer import.halex" -source output\wegverkeer.gml -target output\maatregel.csv -preset CSV -reportsOut output\importwegverkeer.log -trustGroovy 
+
