53bacd37815883965a6bba9203759317ff37b515
Danny Greefhorst <46052032+dgreefhorst@users.noreply.github.com>
2022-03-21 15:02:51 +0100

    Veehouderij import en export

---
+++ hale/importveehouderij.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\wetransform\HALE\jre\bin\java" -jar "C:\Program Files\wetransform\HALE\plugins\org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar" -application hale.transform -project "Veehouderij import.halex" -source output\veehouderij_groot.gml -target output\veehouderij.csv -preset CSV -reportsOut output\importveehouderij.log -trustGroovy 
+
