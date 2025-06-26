76b31b5c1e86d86c76b70848366614309afec30e
Patrick de Vries <patrick.de.vries@rivm.nl>
2025-06-26 16:10:07 +0200

    Added link to changelog

--- respec/src/rivm/templates/headers.js
+++ respec/src/rivm/templates/headers.js
@@ -11,6 +11,7 @@ const localizationStrings = {
     archives: "archives",
     author: "Author:",
     authors: "Authors:",
+    changelog: "Changelog",
     commit_history: "Commit history",
     edited_in_place: "edited in place",
     editor: "Editor:",
@@ -42,13 +43,14 @@ const localizationStrings = {
   nl: {
     author: "Auteur:",
     authors: "Auteurs:",
+    changelog: "Veranderingslogboek",
     editor: "Redacteur:",
     editors: "Redacteurs:",
     latest_editors_draft: "Laatste werkversie:",
     latest_published_version: "Laatst gepubliceerde versie:",
     this_version: "Deze versie:",
     history: "Geschiedenis:",
-    commit_history: "Geschiedenis",
+    commit_history: "Commit-geschiedenis",
     more_details_about_this_doc: "Meer details over dit document",
     feedback: "Wijzigingsverzoeken:",
   },
@@ -113,12 +115,17 @@ export default (conf, options) => {
               <dd><a href="${conf.edDraftURI}">${conf.edDraftURI}</a></dd>
             `
           : ""}
-        ${conf.historyURI || conf.github
+        ${conf.historyURI || conf.changelogUri || conf.github
           ? html`<dt>${l10n.history}</dt>
               ${conf.historyURI
                 ? html`<dd>
                     <a href="${conf.historyURI}">${conf.historyURI}</a>
                   </dd>`
+                : ""}
+              ${conf.changelogUri
+                ? html`<dd>
+                    <a href="${conf.changelogUri}">${l10n.changelog}</a>
+                  </dd>` 
                 : ""}
               ${conf.github
                 ? html`<dd>
